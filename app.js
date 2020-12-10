106576d728721603097a32e69e1a01486b569fc0
Madhukesh kumar <madhukeshk3@gmail.com>
2020-12-11 02:15:52 +0530

    Added Story model

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const path = require('path')
 const express = require('express')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 const connectDB = require('./config/db')
-const { initialize } = require('passport')
 
 // Load config
 dotenv.config({path: './config/config.env'})
@@ -33,6 +34,7 @@ app.use(session({
     secret: 'keyboard cat',
     resave: false,
     saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
   }))
 
 //Passport Middleware
