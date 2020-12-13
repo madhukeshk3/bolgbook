f713aef1805a1ed13dae1789622679cffe8182ae
Madhukesh kumar <madhukeshk3@gmail.com>
2020-12-13 10:05:58 +0530

    Formatted

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,20 @@
-const express = require('express')
-const passport = require('passport')
-const router = express.Router()
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
 
-router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
 
-router.get('/google/callback',passport.authenticate('google', { failureRedirect: '/' }),
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
   (req, res) => {
-    res.redirect('/dashboard')
+    res.redirect("/dashboard");
   }
-)
+);
 
-router.get('/logout',(req,res)=>{
-  req.logOut()
-  res.redirect('/')
-})
+router.get("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/");
+});
 
-module.exports = router
+module.exports = router;
